b3f71615c28b34153fa6927cd410322802f42c4d
Joel Moss <joel@developwithstyle.com>
2023-02-17 14:07:09 +0000

    Phlex::ReactComponent tests

---
+++ test/internal/app/components/phlex/basic_react_component.rb
@@ -0,0 +1,4 @@
+# frozen_string_literal: true
+
+class Phlex::BasicReactComponent < Proscenium::Phlex::ReactComponent
+end
---
+++ test/phlex/react_component_test.rb
@@ -0,0 +1,66 @@
+# frozen_string_literal: true
+
+require_relative '../test_helper'
+require 'phlex/testing/rails/view_helper'
+require 'phlex/testing/capybara'
+
+class Proscenium::Phlex::ReactComponentTest < ActiveSupport::TestCase
+  include Phlex::Testing::Rails::ViewHelper
+  include Phlex::Testing::Capybara::ViewHelper
+
+  setup do
+    Proscenium.reset_current_side_loaded
+  end
+
+  test 'class names' do
+    render Phlex::BasicReactComponent.new
+
+    assert_selector '.componentManagedByProscenium.component8d1943e8'
+  end
+
+  test 'data-component attribute' do
+    render Phlex::BasicReactComponent.new
+    data = JSON.parse(page.find('.componentManagedByProscenium')['data-component'])
+
+    assert_equal(
+      { 'path' => '/app/components/phlex/basic_react_component', 'props' => {}, 'lazy' => true },
+      data
+    )
+  end
+
+  test 'should set lazy as false' do
+    render Phlex::BasicReactComponent.new(lazy: false)
+    data = JSON.parse(page.find('.componentManagedByProscenium')['data-component'])
+
+    assert_equal(
+      { 'path' => '/app/components/phlex/basic_react_component', 'props' => {}, 'lazy' => false },
+      data
+    )
+  end
+
+  test 'should pass through props' do
+    render Phlex::BasicReactComponent.new(props: { name: 'Joel' })
+    data = JSON.parse(page.find('.componentManagedByProscenium')['data-component'])
+
+    assert_equal(
+      { 'path' => '/app/components/phlex/basic_react_component',
+        'props' => { 'name' => 'Joel' },
+        'lazy' => true },
+      data
+    )
+  end
+
+  test 'should contain a div "loading"' do
+    render Phlex::BasicReactComponent.new
+
+    assert_selector 'div>div', text: 'loading...'
+  end
+
+  test 'should accept a block' do
+    skip
+    render(Phlex::BasicReactComponent.new) { span { 'hello' } }
+    pp page.native
+
+    assert_selector 'div>div', text: 'loading...'
+  end
+end
