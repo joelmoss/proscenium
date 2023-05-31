10c5823cc2b0d72d6a908ef467551a259b1c2183
Joel Moss <joel@developwithstyle.com>
2023-05-31 23:11:32 +0100

    fix: Added guard to ensure that Proscenium::Current.loaded is not nil

--- lib/proscenium/side_load/ensure_loaded.rb
+++ lib/proscenium/side_load/ensure_loaded.rb
@@ -5,16 +5,18 @@ class Proscenium::SideLoad
     def self.included(child)
       child.class_eval do
         append_after_action do
-          if Proscenium::Current.loaded[:js].present?
-            raise NotIncludedError, 'There are javascripts to be side loaded, but they have not ' \
-                                    'been included. Did you forget to add the ' \
-                                    '`#side_load_javascripts` helper in your views?'
-          end
+          if Proscenium::Current.loaded
+            if Proscenium::Current.loaded[:js].present?
+              raise NotIncludedError, 'There are javascripts to be side loaded, but they have not ' \
+                                      'been included. Did you forget to add the ' \
+                                      '`#side_load_javascripts` helper in your views?'
+            end
 
-          if Proscenium::Current.loaded[:css].present?
-            raise NotIncludedError, 'There are stylesheets to be side loaded, but they have not ' \
-                                    'been included. Did you forget to add the ' \
-                                    '`#side_load_stylesheets` helper in your views?'
+            if Proscenium::Current.loaded[:css].present?
+              raise NotIncludedError, 'There are stylesheets to be side loaded, but they have not ' \
+                                      'been included. Did you forget to add the ' \
+                                      '`#side_load_stylesheets` helper in your views?'
+            end
           end
         end
       end
