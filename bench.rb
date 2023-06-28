65c548c9130405c42b47ad258caa672f040f8e01
Joel Moss <joel@developwithstyle.com>
2023-06-28 20:04:37 +0100

    chore: Update Ruby benchmak result

--- bench.rb
+++ bench.rb
@@ -14,9 +14,9 @@ path = 'lib/foo.js'
 Benchmark.ips do |x|
   # ruby 3.2.2 (2023-03-30 revision e51014f9c0) +YJIT [arm64-darwin22]
   # Warming up --------------------------------------
-  #     proscenium build   123.000  i/100ms
+  #     proscenium build   138.000  i/100ms
   # Calculating -------------------------------------
-  #     proscenium build      1.233k (± 1.2%) i/s -      6.273k in   5.087255s
+  #     proscenium build      1.380k (± 1.1%) i/s -      6.900k in   5.000274s
   x.report('proscenium build') do
     Proscenium::Builder.new(root: root).build(path)
   end
