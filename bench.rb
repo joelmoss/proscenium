18ca105ffa15433bcf6273357e7d12f7e5c71162
Joel Moss <joel@developwithstyle.com>
2023-06-28 11:39:34 +0100

    Added results of benchmark

--- bench.rb
+++ bench.rb
@@ -12,9 +12,12 @@ root = Pathname.new(__dir__).join('test', 'internal')
 path = 'lib/foo.js'
 
 Benchmark.ips do |x|
+  # ruby 3.2.2 (2023-03-30 revision e51014f9c0) +YJIT [arm64-darwin22]
+  # Warming up --------------------------------------
+  #     proscenium build   123.000  i/100ms
+  # Calculating -------------------------------------
+  #     proscenium build      1.233k (± 1.2%) i/s -      6.273k in   5.087255s
   x.report('proscenium build') do
     Proscenium::Builder.new(root: root).build(path)
   end
-
-  # x.compare!
 end
