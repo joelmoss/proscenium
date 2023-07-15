9031dbee27bbeeeb10cc54b68d84362e033e9298
Joel Moss <joel@developwithstyle.com>
2023-07-15 16:25:35 +0100

    chore: Removed @proscenium/component-manager from tests

--- test/dummy/app/views/layouts/application.js
+++ test/dummy/app/views/layouts/application.js
@@ -1,10 +1,1 @@
-import init from "@proscenium/component-manager";
-
-init({
-  debug: true,
-  buildComponentPath(component) {
-    return `${component}.jsx`;
-  },
-});
-
 console.log("app/views/layouts/application.js");
--- test/dummy/lib/import_component_manager.js
+++
@@ -1,3 +0,0 @@
-import { init } from '@proscenium/component-manager'
-
-init()
