78e6f94d91ebbd9d96d25f8184ebc981951d8505
Joel Moss <joel@developwithstyle.com>
2023-11-10 13:31:36 +0000

    feat: Added class_names helper to CssModules

--- lib/proscenium/css_module.rb
+++ lib/proscenium/css_module.rb
@@ -18,8 +18,16 @@ module Proscenium::CssModule
   # Accepts one or more CSS class names, and transforms them into CSS module names.
   #
   # @param name [String,Symbol,Array<String,Symbol>]
+  # @return [String] the transformed CSS module names concatenated as a string.
   def css_module(*names)
     cssm.class_names(*names, require_prefix: false).map { |name, _| name }.join(' ')
+  end
+
+  # @param name [String,Symbol,Array<String,Symbol>]
+  # @return [String] the transformed CSS module names concatenated as a string.
+  def class_names(*names)
+    names = names.flatten.compact
+    cssm.class_names(*names).map { |name, _| name }.join(' ') unless names.empty?
   end
 
   private
