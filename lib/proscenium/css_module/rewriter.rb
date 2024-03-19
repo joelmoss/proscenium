3598ea7bf3db33961d8398e7dcec089370a6d604
Joel Moss <joel@developwithstyle.com>
2024-03-19 20:34:19 +0000

    fix: Improved CSS rewriter to support symbol array literals

--- lib/proscenium/css_module/rewriter.rb
+++ lib/proscenium/css_module/rewriter.rb
@@ -8,12 +8,28 @@ module Proscenium
     class Rewriter < RubyNext::Language::Rewriters::Text
       NAME = 'proscenium-css-module'
 
-      def safe_rewrite(source)
-        source.gsub(/:@([\w_]+)/) do |_|
-          context.track! self
+      def rewrite(source)
+        source = source.gsub(/%i\[((@[\w@ ]+)|([\w@ ]+ @[\w@ ]+))\]/) do |_|
+          arr = ::Regexp.last_match(1).split.map do |x|
+            x.start_with?('@') ? css_module_string(x[1..]) : ":#{x}"
+          end
+          "[#{arr.join(',')}]"
+        end
 
-          match = ::Regexp.last_match(1)
-          "Proscenium::CssModule::Name.new(:@#{match}, css_module(:#{match}))"
+        source.gsub(/:@([\w]+)/) do |_|
+          context.track!(self)
+          css_module_string(::Regexp.last_match(1))
+        end
+      end
+
+      private
+
+      def css_module_string(name)
+        if (path = Pathname.new(context.path).sub_ext('.module.css')).exist?
+          tname = Transformer.new(path).class_name!(name, name.dup).first
+          "Proscenium::CssModule::Name.new(:@#{name}, '#{tname}')"
+        else
+          "Proscenium::CssModule::Name.new(:@#{name}, css_module(:#{name}))"
         end
       end
     end
