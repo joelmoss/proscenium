75857e12b31d4d9c2735200ca29a0e77809d0b83
Joel Moss <joel@developwithstyle.com>
2024-11-06 22:09:56 +0000

    chore: make rubocop happy

--- playground/db/migrate/20240419222304_user_belongs_to_fruit.rb
+++ playground/db/migrate/20240419222304_user_belongs_to_fruit.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class UserBelongsToFruit < ActiveRecord::Migration[7.1]
   def change
     add_reference :users, :fruit, foreign_key: true
--- playground/db/schema.rb
+++ playground/db/schema.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -10,20 +12,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_19_222304) do
-  create_table "fruits", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+ActiveRecord::Schema[7.1].define(version: 20_240_419_222_304) do
+  create_table 'fruits', force: :cascade do |t|
+    t.string 'name'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.integer "gender"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.integer "fruit_id"
-    t.index ["fruit_id"], name: "index_users_on_fruit_id", unique: true
+  create_table 'users', force: :cascade do |t|
+    t.string 'name'
+    t.integer 'gender'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.integer 'fruit_id'
+    t.index ['fruit_id'], name: 'index_users_on_fruit_id', unique: true
   end
-
 end
