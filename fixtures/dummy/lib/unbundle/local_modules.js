c8a03c94e16d3ab53f5640667f1dccf0464f4a58
Joel Moss <joel@developwithstyle.com>
2025-02-23 19:54:24 +0000

    feat: Support `unbundle` import attribute as an alternative to `unbundle:` prefix

--- fixtures/dummy/lib/unbundle/local_modules.js
+++ fixtures/dummy/lib/unbundle/local_modules.js
@@ -1,4 +1,4 @@
-import "unbundle:/lib/unbundle/foo1.js";
+import "/lib/unbundle/foo1.js" with { unbundle: 'true' }
 import "unbundle:./foo2.js";
 import "/lib/foo3.js";
 import { one } from "unbundle:mypackage/treeshake";
