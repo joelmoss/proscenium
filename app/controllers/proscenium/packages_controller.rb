1a474928daeda179b086b970ed61745b2ae27be0
Joel Moss <joel@developwithstyle.com>
2025-03-26 23:06:32 +0000

    Package controller for bundled packages and rubygems packages

--- app/controllers/proscenium/packages_controller.rb
+++ app/controllers/proscenium/packages_controller.rb
@@ -1,17 +1,16 @@
 # frozen_string_literal: true
 
-require 'rubygems/package'
-
-class PackagesController < ActionController::API
+class Proscenium::PackagesController < ActionController::Base
   rescue_from Proscenium::Registry::PackageUnsupportedError, with: :render_not_found
-  rescue_from Gems::NotFound, with: :render_not_found
+  rescue_from Proscenium::Registry::PackageNotInstalledError, with: :render_not_found
 
-  def index = render json: {}
+  def index
+    render json: {}
+  end
 
   def show
     host = "#{request.protocol}#{request.host_with_port}"
-    render json: Proscenium::Registry.ruby_gem_package(params[:package], params[:version], host:)
-                                     .as_json
+    render json: Proscenium::Registry.bundled_package(params[:package], host:).as_json
   end
 
   private
