9b729ce09020625703574141fa56a72c9a1bb38d
Joel Moss <joel@developwithstyle.com>
2025-03-29 17:11:32 +0000

    Removed external_node_modules config option

--- lib/proscenium/middleware/esbuild.rb
+++ lib/proscenium/middleware/esbuild.rb
@@ -20,12 +20,7 @@ module Proscenium
       end
 
       def attempt
-        bundle = nil
-        if Proscenium.config.external_node_modules && path_to_build.start_with?('node_modules/')
-          bundle = false
-        end
-
-        render_response Builder.build_to_string(path_to_build, bundle:)
+        render_response Builder.build_to_string(path_to_build)
       rescue Builder::CompileError => e
         raise self.class::CompileError, { file: @request.fullpath, detail: e.message }, caller
       end
--- playground/config/application.rb
+++ playground/config/application.rb
@@ -18,8 +18,6 @@ module Playground
   class Application < Rails::Application
     config.load_defaults Rails::VERSION::STRING.to_f
 
-    config.proscenium.external_node_modules = true
-
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
